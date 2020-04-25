c0298c33ead6a0f4f8f4505ce7e323d24ad72c6c
Ngo Thanh Tung <tungnt@softech.vn>
2020-04-26 00:33:42 +0700

    Update UI 02

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import UI01 from './screens/UI01';
+// import UI01 from './screens/UI01';
+import UI02 from './screens/UI02';
 
 const App = () => {
   return (
     <React.Fragment>
-      <UI01 />
+      {/* <UI01 /> */}
+      <UI02 />
     </React.Fragment>
   );
 };
